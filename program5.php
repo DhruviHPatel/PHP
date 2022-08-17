49f7cc1cd3ccf7782d501966e4612347d116ae0b
DhruviHPatel <111412841+DhruviHPatel@users.noreply.github.com>
2022-08-17 12:00:25 +0530

    Update program5.php

--- program5.php
+++ program5.php
@@ -1,7 +1,3 @@
-5.Create an array of three employee names. Create another
-array that associates employee names with their salary values.
-Print both arrays using foreach loop.
-
 <?php
 	$emp = array("Alice" => 10000,"Bob" => 20000,"Mark" => 30000);
 	foreach($emp as $name=>$salary)
@@ -9,8 +5,3 @@ Print both arrays using foreach loop.
 		echo "$name = $salary"."<br>";
 	}
 ?>
-
-Output :
-Alice = 10000
-Bob = 20000
-Mark = 30000
